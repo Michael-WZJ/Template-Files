253a02f2822b5a167e326609989e1666910343ed
Zhijun Wan <72848364+Michael-WZJ@users.noreply.github.com>
2023-02-01 19:19:26 +0800

    Update template.c

--- C/code/template.c
+++ C/code/template.c
@@ -2,6 +2,12 @@
 #include <stdbool.h> // standard boolean library: bool, true, false
 #include <stdlib.h>  // library that contains malloc, rand, and srand
 #include <time.h>    // time functions
+#include <string.h>
+#include <math.h>
+
+#include <assert.h>
+#include <ctype.h>   // http://c.biancheng.net/ref/ctype_h/
+
 
 int main(void)
 {
