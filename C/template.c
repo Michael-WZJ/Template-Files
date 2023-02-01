126a7f80983e1babad2593cb6bdc53a7648cb0cb
Michael <a379605996z@gmail.com>
2023-02-01 16:52:16 +0800

    C language

---
+++ C/template.c
@@ -0,0 +1,10 @@
+#include <stdio.h>   // stardard input/output library
+#include <stdbool.h> // standard boolean library: bool, true, false
+#include <stdlib.h>  // library that contains malloc, rand, and srand
+#include <time.h>    // time functions
+
+int main(void)
+{
+    printf("Hello, world!\n");
+    return 0;
+}
